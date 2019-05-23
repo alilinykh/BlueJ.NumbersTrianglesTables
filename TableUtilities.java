5ee8f03d6d8e23387567a31f113b268241453554
alilinykh <aililnykh@dtcc.edu>
2019-05-23 15:07:15 -0400

    completed lab

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,57 @@
- 
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String result = "";
+        for (int i = 1; i < 6; i++) {
+            for(int j = 1; j <= 5; j++){
+                if (i * j > 9) {
+                    result += " " + i*j +" |";
+                }
+                else {
+                result += "  " + i*j +" |";
+            }
+        }
+        result += "\n";
     }
+    return result;
+}
+
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String result = "";
+        for (int i = 1; i < 11; i++) {
+            for(int j = 1; j <= 10; j++){
+                if (i * j > 99) {
+                    result += i*j +" |";
+                }
+                else if (i * j < 100 && i*j > 9) {
+                    result += " " + i*j +" |";
+                }
+                else {
+                result += "  " + i*j +" |";
+            }
+        }
+        result += "\n";
+    }
+    return result;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+        for (int i = 1; i <= tableSize; i++) {
+            for(int j = 1; j <= tableSize; j++){
+                if (i * j > 99) {
+                    result += i*j +" |";
+                }
+                else if (i * j < 100 && i*j > 9) {
+                    result += " " + i*j +" |";
+                }
+                else {
+                result += "  " + i*j +" |";
+            }
+        }
+        result += "\n";
+    }
+    return result;
     }
 }
